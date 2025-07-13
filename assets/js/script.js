620438e385077c50f753f25e00f8ce5973e1c297
jos.silvav <jos.silvav@gmail.com>
2025-07-13 13:49:19 -0400

    add en js funciones para renderizar moneda y el conversor

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ const inputMonto = document.getElementById('monto');
 const selectMoneda = document.getElementById('moneda');
 const btnBuscar = document.getElementById('btnBuscar');
 const pResultado = document.getElementById('resultado');
+const pError = document.getElementById('error');
 
 const baseUrl = 'https://mindicador.cl/api/';
 
+//funcion para renderizar el nombre de las monedas
 async function getMonedas() {
     try {
         const res = await fetch (baseUrl);
@@ -22,4 +24,39 @@ async function getMonedas() {
     } 
 }
 
+//Funcion para la conversion de monedas
+
+async function convertirMoneda () {
+    try {
+        const montoPesos = Number(inputMonto.value);
+        const codigoMoneda = selectMoneda.value;
+
+        pResultado.textContent = '';
+        pError.textContent = '';
+
+        if(!codigoMoneda) {
+            pError.textContent = 'Por favor seleccione una moneda';
+            pResultado.textContent = '';
+            return
+        } 
+
+        const res = await fetch(`https://mindicador.cl/api/${codigoMoneda}`);
+        const data = await res.json();
+        const valorMoneda = data.serie[0].valor;
+
+        const resultado = montoPesos / valorMoneda
+
+        pResultado.textContent = `$${montoPesos.toLocaleString('es-CL')} pesos chilenos son aproximadamente ${resultado.toFixed(2)} ${codigoMoneda}`
+
+    } catch (error) {
+        pResultado.textContent = '';
+        pError.textContent = 'Ocurrió un error al realizar la conversión';
+        console.log(error)
+    }
+
+}
+
 getMonedas();
+
+btnBuscar.addEventListener('click', convertirMoneda);
+
