8a698b20058aa59bc944dd52f958e145ee802de6
jos.silvav <jos.silvav@gmail.com>
2025-07-12 23:16:19 -0400

    add api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-const inputMonto = document.getElementById("monto");
-const selectMoneda = document.getElementById("moneda");
-const btnBuscar = document.getElementById("btnBuscar");
-const pResultado = document.getElementById("resultado");
+const inputMonto = document.getElementById('monto');
+const selectMoneda = document.getElementById('moneda');
+const btnBuscar = document.getElementById('btnBuscar');
+const pResultado = document.getElementById('resultado');
 
+const baseUrl = 'https://mindicador.cl/api/';
+
+async function getMonedas() {
+    try {
+        const res = await fetch (baseUrl);
+        const monedas = await res.json();
+        const monedasPermitidas = ['dolar', 'uf','utm'];
+        monedasPermitidas.forEach(codigo => {
+            const moneda = monedas[codigo];
+            const option = document.createElement('option');
+            option.value = codigo;
+            option.textContent = moneda.nombre;
+            selectMoneda.appendChild(option)
+        });
+    } catch (error) {
+        console.log(error);
+    } 
+}
+
+getMonedas();
