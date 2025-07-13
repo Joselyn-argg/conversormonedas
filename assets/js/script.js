e598e6a3959abeb0b77b06efb51d2eaf273bca8e
jos.silvav <jos.silvav@gmail.com>
2025-07-12 22:19:03 -0400

    add html and js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const inputMonto = document.getElementById("monto");
+const selectMoneda = document.getElementById("moneda");
+const btnBuscar = document.getElementById("btnBuscar");
+const pResultado = document.getElementById("resultado");
+
